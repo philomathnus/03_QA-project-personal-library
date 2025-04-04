1552beb3c9c73a65831a1946ddd20de96f2e09c4
philomathnus <13217349+philomathnus@users.noreply.github.com>
2025-04-04 08:56:18 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,41 +7,18 @@
 */
 
 'use strict';
+const {getAllBooks, getBookById, createBook, updateBook, deleteBook, deleteAllBooks} = require('../controllers/bookController');
 
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
-    })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
-    });
-
-
+    .get(getAllBooks)
+    .post(createBook)
+    .delete(deleteAllBooks);
 
   app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-    })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
-    })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
-    });
+    .get(getBookById)  
+    .post(updateBook)
+    .delete(deleteBook);
   
 };
